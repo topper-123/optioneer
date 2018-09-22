f9eabe1e452a1abec0ceca64f4e17ff9211c11f6
tp <contribute@tensortable.com>
2018-09-22 09:11:42 +0100

    Remove history.rst requirement in setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,6 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = [ ]
 
 setup_requirements = ['pytest-runner', ]
@@ -36,7 +33,7 @@ setup(
     description="Nestable, tab-able options for use in Python programs",
     install_requires=requirements,
     license="BSD license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='optioneer',
     name='optioneer',
